27b665774c872da89b51b1b6c9250a79e1673081
ccp84 <ccp84@icloud.com>
2023-05-09 11:11:51 +0000

    js>add react router

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,26 +2,29 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import logo from "../assets/logo.webp";
 
 const Header = () => {
 	return (
-		<Navbar bg="dark" variant="dark">
+		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
 			<Container>
-				<Navbar.Brand href="#home">Navbar</Navbar.Brand>
-				<Nav className="me-auto">
-					<Nav.Link className="text-warning" href="#home">
-						Home
-					</Nav.Link>
-					<Nav.Link className="text-warning" href="#features">
-						Signup
-					</Nav.Link>
-					<Nav.Link className="text-warning" href="#features">
-						Login
-					</Nav.Link>
-					<Nav.Link className="text-warning" href="#pricing">
-						Logout
-					</Nav.Link>
-				</Nav>
+				<Navbar.Brand href="#home">
+					<img src={logo} alt="logo" height="45" />
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="mr-auto">
+						<Nav.Link className="text-warning" href="signin">
+							Login
+						</Nav.Link>
+						<Nav.Link className="text-warning" href="#pricing">
+							Logout
+						</Nav.Link>
+						<Nav.Link className="text-warning" href="#pricing">
+							Signup
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
 			</Container>
 		</Navbar>
 	);
