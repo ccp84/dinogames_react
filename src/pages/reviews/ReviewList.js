458b2f1c6ff335f2e230f3922867b6e3488a2c86
ccp84 <ccp84@icloud.com>
2023-05-29 10:11:25 +0000

    js>add delete button to reviews

--- src/pages/reviews/ReviewList.js
+++ src/pages/reviews/ReviewList.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
+import {
+  Alert,
+  Button,
+  Dropdown,
+  DropdownButton,
+  ListGroup,
+  ListGroupItem,
+  Stack,
+} from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useQueryClient } from "@tanstack/react-query";
 
 const ReviewList = (props) => {
-
+  const queryClient = useQueryClient();
   return (
     <>
+      {/* If list is empty */}
       {!props.reviews.length ? (
         <ListGroup>
           <ListGroupItem>
@@ -26,11 +37,40 @@ const ReviewList = (props) => {
                       icon={`fa-solid fa-${review.profileicon}`}
                     />
                   </p>
+                  {/* If author give edit and delete options */}
+                  {review.is_author ? (
+                    <>
+                      <Stack direction="horizontal" gap={3}>
+                        <DropdownButton
+                          id="dropdown-basic-button"
+                          title="Delete"
+                          variant="danger"
+                        >
+                          <Dropdown.Item
+                            onClick={async () => {
+                              try {
+                                await axiosReq.delete(`/reviews/${review.id}`);
+                                // Invalidate query and refetch data
+                                queryClient.invalidateQueries({
+                                  queryKey: ["reviewData"],
+                                });
+                              } catch (err) {
+                                <Alert>{err.message}</Alert>;
+                              }
+                            }}
+                          >
+                            Confirm Delete
+                          </Dropdown.Item>
+                        </DropdownButton>
+                        <Button variant="info">Edit</Button>
+                      </Stack>
+                    </>
+                  ) : null}
                 </ListGroupItem>
               );
             })}
           </ListGroup>
-      </>
+        </>
       )}
     </>
   );
